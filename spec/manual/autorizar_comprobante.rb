6787cfebb24bed1d70760f0f55aa36d1e530ca00
Emmanuel Nicolau <emma@ketsui.com.ar>
2011-01-15 15:07:58 -0300

    Cambio las key para testing y paso las pruebas manuales a spec/manual

--- spec/manual/autorizar_comprobante.rb
+++ spec/manual/autorizar_comprobante.rb
@@ -1,22 +1,13 @@
 $LOAD_PATH.unshift(File.expand_path(File.join(File.dirname(__FILE__), '../../lib')))
 require 'afipws'
 
+Savon.configure { |config| config.log = true }
+
 ws = Afipws::WSFE.new :env => :dev, :cuit => '20300032673', 
   :cert => File.read(File.dirname(__FILE__) + '/test.crt'), 
   :key => File.read(File.dirname(__FILE__) + '/test.key') 
-# p ws.tipos_monedas
-# p ws.cotizacion 'DOL'
 
 ultimo = ws.ultimo_comprobante_autorizado :pto_vta => 1, :cbte_tipo => 1
-# xml = Builder::XmlMarkup.new indent: 2
-# xml.ar :Auth do
-#   xml.ar :Token, ws.ta[:token]
-#   xml.ar :Sign, ws.ta[:sign]
-#   xml.ar :Cuit, ws.cuit
-# end
-# puts xml.target!
-
-Savon.configure { |config| config.log = true }
 puts ws.autorizar_comprobantes(:cbte_tipo => 1, :pto_vta => 1, :comprobantes => [{:cbte_nro => ultimo + 1, :concepto => 1, 
   :doc_nro => 30521189203, :doc_tipo => 80, :cbte_fch => Date.new(2011,01,13), 
   :imp_total => 1270.48, :imp_neto => 1049.98, :imp_iva => 220.50, :mon_id => 'PES', :mon_cotiz => 1,
