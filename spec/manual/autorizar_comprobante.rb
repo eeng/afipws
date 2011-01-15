ab162682ce8fff8e4443fc2084d5cc0d60955cf7
Emmanuel Nicolau <emma@ketsui.com.ar>
2011-01-15 15:15:31 -0300

    Test manual para autorizar 2 comprobantes, el segundo con error en iva

--- spec/manual/autorizar_comprobante.rb
+++ spec/manual/autorizar_comprobante.rb
@@ -8,8 +8,15 @@ ws = Afipws::WSFE.new :env => :dev, :cuit => '20300032673',
   :key => File.read(File.dirname(__FILE__) + '/test.key') 
 
 ultimo = ws.ultimo_comprobante_autorizado :pto_vta => 1, :cbte_tipo => 1
-puts ws.autorizar_comprobantes(:cbte_tipo => 1, :pto_vta => 1, :comprobantes => [{:cbte_nro => ultimo + 1, :concepto => 1, 
-  :doc_nro => 30521189203, :doc_tipo => 80, :cbte_fch => Date.new(2011,01,13), 
-  :imp_total => 1270.48, :imp_neto => 1049.98, :imp_iva => 220.50, :mon_id => 'PES', :mon_cotiz => 1,
-  :iva => { :alic_iva => [{ :id => 5, :base_imp => 1049.98, :importe => 220.50 }]}
-}])
+puts ws.autorizar_comprobantes(:cbte_tipo => 1, :pto_vta => 1, :comprobantes => [
+  { 
+    :cbte_nro => ultimo + 1, :concepto => 1, :doc_nro => 30521189203, :doc_tipo => 80, :cbte_fch => Date.new(2011,01,13), 
+    :imp_total => 1270.48, :imp_neto => 1049.98, :imp_iva => 220.50, :mon_id => 'PES', :mon_cotiz => 1,
+    :iva => { :alic_iva => [{ :id => 5, :base_imp => 1049.98, :importe => 220.50 }]}
+  },
+  { 
+    :cbte_nro => ultimo + 2, :concepto => 1, :doc_nro => 30521189203, :doc_tipo => 80, :cbte_fch => Date.new(2011,01,13), 
+    :imp_total => 1270.48, :imp_neto => 1049.98, :imp_iva => 220.50, :mon_id => 'PES', :mon_cotiz => 1,
+    :iva => { :alic_iva => [{ :id => 5, :base_imp => 1049.98, :importe => 220.60 }]}
+  }
+])
