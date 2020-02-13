3420a3f717f969faded0b5c1e46feff00a44b40e
mlabat <mauro.labattaglia@snappler.com>
2020-02-13 15:57:13 -0300

    Add services a5 (#11)

---
+++ lib/afipws/persona_service_a5.rb
@@ -0,0 +1,35 @@
+module Afipws
+  class PersonaServiceA5
+    WSDL = {
+      development: 'https://awshomo.afip.gov.ar/sr-padron/webservices/personaServiceA5?WSDL',
+      production: 'https://aws.afip.gov.ar/sr-padron/webservices/personaServiceA5?WSDL',
+      test: Root + '/spec/fixtures/ws_sr_padron_a5.wsdl'
+    }.freeze
+
+
+    attr_reader :wsaa
+
+    def initialize options = {}
+      @cuit = options[:cuit]
+      @wsaa = WSAA.new options.merge(service: 'ws_sr_padron_a5')
+      @client = Client.new Hash(options[:savon]).reverse_merge(wsdl: WSDL[@wsaa.env], soap_version: 1)
+    end
+
+    def dummy
+      request(:dummy)[:return]
+    end
+
+    def get_persona id
+      message = @wsaa.auth.merge(cuitRepresentada: @cuit, idPersona: id)
+      request(:get_persona, message)[:persona_return][:datos_generales]
+    end
+
+    private
+
+    def request action, body = nil
+      @client.request(action, body).to_hash[:"#{action}_response"]
+    rescue Savon::SOAPFault => f
+      raise WSError, f.message
+    end
+  end
+end
---
+++ spec/afipws/persona_service_a5_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+module Afipws
+  describe PersonaServiceA5 do
+    let(:ta) { {token: 't', sign: 's'} }
+    let(:ws) { PersonaServiceA5.new(cuit: '12345678912').tap { |ws| ws.wsaa.stubs auth: ta } }
+    let(:message) { ta.merge cuitRepresentada: '12345678912' }
+
+    context 'métodos API' do
+      it 'dummy' do
+        savon.expects(:dummy).returns(fixture('ws_sr_padron_a5/dummy/success'))
+        ws.dummy.should == { appserver: 'OK', authserver: 'OK', dbserver: 'OK' }
+      end
+
+      it 'get_persona' do
+        savon.expects(:get_persona)
+          .with(message: message.merge(idPersona: '98765432198'))
+          .returns(fixture('ws_sr_padron_a5/get_persona/success'))
+        ws.get_persona('98765432198').should include apellido: 'ALEXA'
+      end
+    end
+  end
+end
